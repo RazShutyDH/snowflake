afbf54144d8d487662782206e0f5f3ce6bc73363
RazShutyDH <raz.shuty@deliveryhero.com>
2020-03-11 18:13:23 +0100

    update site

--- docs/_next/52a26f4e-bb6c-492c-a4c3-57e72a24df59/page/_error/index.js
+++ docs/_next/52a26f4e-bb6c-492c-a4c3-57e72a24df59/page/_error/index.js
@@ -1,6 +1,6 @@
 
           window.__NEXT_REGISTER_PAGE('/_error', function() {
-            var comp = module.exports=webpackJsonp([3],[],[234]);
+            var comp = module.exports=webpackJsonp([3],[],[235]);
             return { page: comp.default }
           })
         
